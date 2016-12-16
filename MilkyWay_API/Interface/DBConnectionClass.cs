3ad8951ed531ff7f92181de240086869b866c6f1
GxMilkyWay <talk2nishant@gmail.com>
2016-12-16 19:31:47 +0530

    IDB Connection

---
+++ MilkyWay_API/Interface/DBConnectionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DapperDEmoInASP.NET.Interface
+{
+    public class DBConnectionClass : IDBConnectionClass
+    {
+
+        public IDbConnection OpenConnection()
+        {
+            IDbConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBstring"].ConnectionString);
+            connection.Open();
+            return connection;
+        }
+
+    }
+}
---
+++ MilkyWay_API/Interface/IDBConnectionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace DapperDEmoInASP.NET.Interface
+{
+    interface IDBConnectionClass
+    {
+        IDbConnection OpenConnection();
+    }
+}
+
+////for calling 
+// IDBConnectionClass cc = new DBConnectionClass();
+
+//        public void abc()
+//        {
+//            //using (IDbConnection db = StaticConnClass.OpenConnection())
+//            using (IDbConnection db = cc.OpenConnection())
+//            {
+//                var param = new DynamicParameters();
+//                string query = @"insert into TblCustomer (CustomerName,CustomerAddress,EmailId,MobileNo,CountryID,StateID) values('sa','Bhopal','nausad@gmail.com','12345678',1,2)";
+//                int id = db.Execute(query);
+
+//            }
+
+//        }
